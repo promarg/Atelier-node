4a826d3aef17b65645d3d22323331ddf70d7b9f5
El bouslamti Omar <elbouslamti.omar@gmail.com>
2019-03-20 11:58:40 +0100

    first commit

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Rémy teyssier'));
+console.log(chalk.red('Emilie '));
+console.log(chalk.green('Valentin'));
+console.log(chalk.cyan('Eliot'));
